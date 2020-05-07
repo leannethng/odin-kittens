c26b39b214fb62ff4602a8cc55a966821f60f66d
leannethng <leanne.thng@gmail.com>
2020-05-07 11:02:40 -0700

    Create new kitten functionality

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -9,17 +9,39 @@ class KittensController < ApplicationController
   def show
     respond_to do |format|
       format.html
+    end 
+  end
+
+  def new
+   @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    # if the save doesn't validate we render the form through 'new'
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render 'new'
     end
   end
+    
+
 
   private
 
     def get_kitten
       @kitten = Kitten.find(params[:id])
-    end  
+    end
 
+    # This is maybe not the best implementation
     def get_kitten_columns
       kitten_keys = [:name, :age, :cuteness, :softness]
       @kitten_columns = kitten_keys
+    end
+
+    # these are the fields we can edit
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
     end
 end
