82574f816b16dab14b326df17a82038952716317
leannethng <leanne.thng@gmail.com>
2020-05-10 16:16:58 -0700

    added bootstrap - js not working, extra classes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+//= require("@rails/ujs").start()
+//= require("turbolinks").start()
+//= require("@rails/activestorage").start()
+//= require("channels")
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +22,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+$(document).ready(function() {
+  $('.alert').alert('close')
+});
