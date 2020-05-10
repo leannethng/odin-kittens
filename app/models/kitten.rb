9b397c6729b352ffad61b487df917f9c35f93c29
leannethng <leanne.thng@gmail.com>
2020-05-09 17:12:21 -0700

    Styled the index page with tachyons

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,6 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, presence: true
-  validates :age, presence: true, format: { with: /\A\d+\z/, message: "can only be a number." }
+  validates :age, presence: true, format: { with: /\A\d+\z/, message: "can only be a number" }
   validates :cuteness, presence: true
   validates :softness, presence: true
 end
