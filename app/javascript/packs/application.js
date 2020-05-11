8b0037efad5edf7bf8ff58c7ed7030bd6f79412f
leannethng <leanne.thng@gmail.com>
2020-05-10 20:28:44 -0700

    delete not working, more syling added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@
 //= require("turbolinks").start()
 //= require("@rails/activestorage").start()
 //= require("channels")
-
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
 import "bootstrap"
